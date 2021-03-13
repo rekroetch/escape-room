949aba8d765bf386a33ace5bfef4a635ab5d38c7
Nicholas Werner <ncwerner@gmail.com>
2021-03-13 14:00:08 -0700

    updated NavBar

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import Bookshelf from './scenes/Bookshelf'
 import Painting from './scenes/Painting'
 import Desk from './scenes/Desk'
 import Safe from './scenes/Safe'
+import ScoreBoard from './scenes/ScoreBoard'
 import Navbar from './components/Navbar'
 
 function App() {
@@ -22,6 +23,7 @@ function App() {
         <Route exact path="/painting" component={Painting} />
         <Route exact path="/desk" component={Desk} />
         <Route exact path="/safe" component={Safe} />
+        <Route exact path="/scoreBoard" component={ScoreBoard} />
     </Router>
   );
 }
---
+++ client/src/scenes/ScoreBoard/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ScoreBoard () {
+    return (
+        <h1>Score Board</h1>
+    )
+}
+
+export default ScoreBoard
