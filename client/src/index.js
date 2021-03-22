bcb3c47e6bbdad1c497b1b98f187f1ead297f22d
Nicholas Werner <ncwerner@gmail.com>
2021-03-22 12:00:42 -0600

    updated styling for office

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import 'bootstrap/dist/css/bootstrap.min.css'
 import "./index.css";
 import App from "./App";
-import 'bootstrap/dist/css/bootstrap.min.css'
 import registerServiceWorker from "./registerServiceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
--- client/src/scenes/Office/index.js
+++ client/src/scenes/Office/index.js
@@ -1,64 +1,72 @@
-import React from 'react';
-import { Link, Route } from 'react-router-dom';
-import Row from "../../components/Row"
-import "./style.css"
+import React from "react";
+import { Link, Route } from "react-router-dom";
+import "./style.css";
 
-function Office (props) {
-    return (
-        <div className="page">
-            <h1>Office</h1>
-            <Row>
-                <div className="desk">
-                    <Route render={() => (
-                        props.puzzle[1].isSolved ? (
-                            <Link
-                                to="/desk"
-                                className="nav-link word"
-                            >
-                                Desk (unlocked)
-                            </Link>
-                        ) : (
-                            <Link
-                                to="/office"
-                                className="nav-link word"
-                            >
-                                Desk (locked)
-                            </Link>)
-                    )}/>
-                </div>
-                <div className="painting">
-                    <Route render={() => (
-                        props.puzzle[2].isSolved ? (
-                            <Link
-                                to="/safe"
-                                className="nav-link word"
-                            >
-                                Safe
-                            </Link>
-                        ) : (
-                            <Link
-                                to="/painting"
-                                className="nav-link word"
-                            >
-                                Painting
-                            </Link>
-                        )
-                    )}/>
-                </div>
-                <div className="bookshelf">
-                    <Link
-                        to="/bookshelf"
-                        className="nav-link word"
-                    >
-                        Bookshelf
-                    </Link>
-                </div>    
-            </Row>
-        </div>
-        
-    )
+function Office(props) {
+  return (
+    <div
+      className="page"
+      style={{
+        position: "relative",
+      }}
+    >
+      <div
+        className="desk"
+        style={{
+          position: "absolute",
+          bottom: "150px",
+          left: "500px"
+        }}
+      >
+        <Route
+          render={() =>
+            props.puzzle[1].isSolved ? (
+              <Link to="/desk" className="nav-link word">
+                Desk (unlocked)
+              </Link>
+            ) : (
+              <Link to="/office" className="nav-link word"></Link>
+            )
+          }
+        />
+      </div>
+      <div
+        className="painting"
+        style={{
+          position: "absolute",
+          right: "0",
+        }}
+      >
+        <Route
+          render={() =>
+            props.puzzle[2].isSolved ? (
+              <Link to="/safe" className="nav-link word">
+                Safe
+              </Link>
+            ) : (
+              <Link to="/painting" className="nav-link word"></Link>
+            )
+          }
+        />
+      </div>
+      <div
+        className="clock"
+        style={{
+          position: "absolute",
+          left: "10px",
+        }}
+      ></div>
+      <div
+        className="bookshelf"
+        style={{
+          position: "absolute",
+          bottom: "112px",
+        }}
+      >
+        <Link to="/bookshelf" className="nav-link word"></Link>
+      </div>
+    </div>
+  );
 }
 
-export default Office
-
-
+export default Office;
