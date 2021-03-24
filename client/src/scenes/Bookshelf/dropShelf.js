5c9bf69adde513771c3aca482f3a3344c67ec257
Pirooz Wallace <PiroozWallace@outlook.com>
2021-03-23 22:19:12 -0700

    fixed bookshelf win bug
    global variable to state variable

--- client/src/scenes/Bookshelf/dropShelf.js
+++ client/src/scenes/Bookshelf/dropShelf.js
@@ -2,9 +2,10 @@ import React from 'react';
 import { DropTarget } from 'react-drag-drop-container';
 import API from "../../utils/API";
 
-var howManyBooks = 0
+
 
  class Shelf extends React.Component {
+
     constructor(props) {
         super(props);
         this.state = {
@@ -16,12 +17,12 @@ var howManyBooks = 0
 
     dropped = (e) => {
         e.containerElem.style.visibility="hidden";
-        this.setState({ howManyBooks: this.howManyBooks+1})
 
-        howManyBooks++
+        this.setState({ howManyBooks: this.state.howManyBooks+1})
+ 
         const Swal = require("sweetalert2");
 
-        if(howManyBooks > 1)
+        if(this.state.howManyBooks > 1)
         {
             const bookShelfPuzzle = this.props.puzzle[1];
             const userId = this.props.user.id;
@@ -44,7 +45,6 @@ var howManyBooks = 0
         >
 
             <div className='bookshelfPuzzle'>
-                    {this.state.message}
           
                 {this.props.children}
                 </div>
