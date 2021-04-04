6bf7bf3572d3c7a48cb98372c0da39a73036ad14
Rachel Kroetch <rekroetch@ucdavis.edu>
2021-04-03 17:55:26 -0700

    updated styling on start page, signpost added

--- client/src/scenes/StartPage/index.js
+++ client/src/scenes/StartPage/index.js
@@ -3,40 +3,6 @@ import { Link } from "react-router-dom";
 import Row from "../../components/Row"
 import Col from "../../components/Col"
 import "./style.css"
-import styled from "styled-components"
-import { VFXProvider, VFXSpan} from 'react-vfx';
-
-const Button = styled.button`
-background-color: #FF6760;
-font-family: courier;
-text-transform: uppercase;
-border-radius: 18px;
-margin: 18px 20px;
-cursor: pointer;
-box-shadow: 0px 20px 20px lightblue;
-height:45px;
-width:244px;
-position: absolute;
-left:16%;
-`;
-
-export const pixelateTransition = `
-precision mediump float;
-uniform vec2 resolution;
-uniform vec2 offset;
-uniform float time;
-uniform float enterTime;
-uniform sampler2D src;
-void main (void) {
-    vec2 uv = (gl_FragCoord.xy - offset) / resolution;
-    if (enterTime < 1.5) {
-        float t = enterTime / 1.5;
-        float b = floor(t * 64.);
-        uv = (floor(uv * b) + .5) / b;
-    }
-    gl_FragColor = texture2D(src, uv);
-}
-`
 
 function StartPage (props) {
 
@@ -71,64 +37,42 @@ function StartPage (props) {
       </div>
       <Row>
         <Col size="md-8">
-          <VFXProvider>
             <div className="info-card">
               <div className="card-body">
-                <h5 className="card-title title">You've been captured!</h5>
+                <h5 className="card-title">You've been captured!</h5>
                 <p className="card-text">
-                <VFXSpan shader = {pixelateTransition}>
                   The killer has knocked you unconscious while you're away from your friends. He drags you through the woods..
-                </VFXSpan>  
                 </p>
                 <br></br>
                 <p className="card-text">
-                <VFXSpan shader = {pixelateTransition}> 
                   You wake up, and you're tied up in the killer's office. He's about to deliver the killing blow, until he hears noise outside. It's your friends. This is your chance..
-                </VFXSpan>
+
                 </p>
                 <br></br>
                 <p className="card-text">
-                <VFXSpan shader = {pixelateTransition}> 
                   You are left all alone, and you have untied yourself. Your objective is to look around the office. It's up to you to get out, get your friends, and...
-                </VFXSpan>
                 </p>
                 <br></br>
                 <p className="card-text">
-                <VFXSpan shader = {pixelateTransition}>
                   ESCAPE! Before it's too late..
-                </VFXSpan>
                 </p>
                 <div className="startBtn">
-                  <VFXProvider>    
-                  <Button>
                     <Link
                         to="/office"
-                        className="start"
+                        style={{ textDecoration: 'none' }}
                     >
-                      {/* <VFXSpan shader = {pixelateTransition}> */}
-                      <div className="resume-start">
+                      <div className="start">
                       {progress > 0+"%" ? "RESUME" : "START GAME"}
                       </div>
-                      {/* </VFXSpan> */}
                     </Link>
-                  </Button>
-                  </VFXProvider>
                 </div>
-
               </div>
             </div>
-          </VFXProvider>
         </Col>
       </Row>
       <div className="night-background">
                 <div className="forest-start">
                     <div className="moon"></div>
-                    {/* <div className="tree01-start"></div>
-  	                <div className="tree02-start"></div>
-  	                <div className="tree03-start"></div>
-                    <div className="tree04-start"></div> */}
-  {/* <!-- combi-container end --> */}
-  
                 </div>
 {/* <!-- forest end --> */}
                 <div className="forest-background-start"></div>
